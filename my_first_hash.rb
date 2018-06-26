40b7bc1eac6d51d8ff662ad1bbb00617a8d6208b
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-26 22:36:49 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash = {name => "tahseen"}
+my_hash = {"name" => "tahseen", "age"=>32}
 
 end
 
